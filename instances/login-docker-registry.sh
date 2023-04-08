d839bdafbb41160e97807752754bef04040b0a28
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-04-08 15:06:43 +0100

    PT-42 #comment chore: add docker registry login script

--- instances/login-docker-registry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-registry_url="${REGISTRY_URL}"
-
-username="${REGISTRY_ID}"
-password="${REGISTRY_PASSWORD}"
-
-echo "$password" | docker login -u "$username" "$registry_url" --password-stdin
