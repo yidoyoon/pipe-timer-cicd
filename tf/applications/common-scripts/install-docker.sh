442adf17ab2486accd3253b787b2bb7bdbc7f653
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-04-29 03:31:38 +0100

    PT-111 #comment chore: move commonly used scripts in application to common-scripts directory

--- tf/applications/common-scripts/install-docker.sh
+++ tf/applications/common-scripts/install-docker.sh
@@ -1,8 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
 if ! docker --version; then
-  sudo apt update
-  sleep 10
+  retry_count=1
+  while ! sudo apt update; do
+    echo "Failed to update package list, retrying in 5 seconds... (attempt $retry_count)"
+    sleep 5
+    retry_count=$((retry_count+1))
+    if [ $retry_count -gt 10 ]; then
+      echo "Failed to update package list after 10 attempts, exiting..."
+      exit 1
+    fi
+  done
   sudo apt-get -y install ca-certificates curl gnupg
   sudo mkdir -m 0755 -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
--- tf/applications/frontend/shell-scripts/login-docker-registry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-registry_url="${1:-${REGISTRY_URL}}"
-
-username="${2:-${REGISTRY_ID}}"
-password="${3:-${REGISTRY_PASSWORD}}"
-
-echo "$password" | docker login -u "$username" "$registry_url" --password-stdin
